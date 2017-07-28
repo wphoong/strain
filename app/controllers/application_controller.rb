0ca00b9e935f1545b3f69a7e0b42a54e67e0b58a
Wilson Phoong <wphoong@gmail.com>
2017-07-28 01:51:54 -0700

    configured devise to save added column

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # before_action :configure_devise_permitted_parameters, if: :devise_controller?
+
+  # def configure_devise_permitted_parameters
+  #   registration_params = [:name, :email, :password, :password_confirmation]
+  #
+  #   if params[:action] == 'update'
+  #     devise_parameter_sanitizer.for(:account_update) do
+  #       |u| u.permit(registration_params << :current_password)
+  #     end
+  #   elsif params[:action] == 'create'
+  #     devise_parameter_sanitizer.for(:sign_up) do
+  #       |u| u.permit(registration_params)
+  #     end
+  #   end
+  # end
+
 end
