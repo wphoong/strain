a47e20b225ba0fdb45e21a44c84f60b55c27e16c
Wilson Phoong <wphoong@gmail.com>
2017-08-02 22:14:35 -0700

    fixed schema

--- db/migrate/20170803042255_create_products.rb
+++ db/migrate/20170803042255_create_products.rb
@@ -1,15 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
-      t.string :strain
-      t.string :type
-      t.text :description
-      t.text :effects
-      t.integer :user_id
-      t.integer :store_id
       t.timestamps
     end
-    add_index :products, [:user_id, :store_id]
-    add_index :products, :store_id
   end
 end
