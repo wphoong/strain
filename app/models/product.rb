faeb84839cde7ab72e248b8cdd0574d84fcc09cf
Wilson Phoong <wphoong@gmail.com>
2017-08-04 17:09:14 -0700

    restricted strain types to a dropdown menu

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,11 @@ class Product < ApplicationRecord
   belongs_to :store
   belongs_to :user
 
+  STRAINTYPES = ['Sativa', 'Indica', 'Hybrid']
+
   validates :strain, presence: true
   validates :description, presence: true
   validates :straintype, presence: true
   validates :effects, presence: true
+
 end
